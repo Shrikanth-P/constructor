eae32e9fc559beceb568497836a8c64152a04a86
Shrikanth-p <shrikanthchoudary05@gmail.com>
2022-10-05 21:39:31 +0530

    Add project files.

---
+++ constructor/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace constructors
+{
+    class Addition
+    {
+        public Addition(int num1, int num2)
+        {
+            Console.WriteLine("Constructor overloading using integer parameters values is : " + (num1+num2));
+        }
+        public Addition(float num1, float num2)
+        {
+            Console.WriteLine("Constructor overloading using floating parameters values is : " + (num1 + num2));
+        }
+        public Addition(string firstName, string lastName)
+        {
+            Console.WriteLine("Constructor overloading using string parameters : " + (firstName + lastName));
+        }
+    }
+    class constructorsOverloading
+    {
+        public static void Main()
+        {
+            Addition addint = new Addition(12,24);
+            Addition addfloat = new Addition(10.5f, 24.5f);
+            Addition addString = new Addition("Shrikanth","Choudary");
+        }
+    }
+}
